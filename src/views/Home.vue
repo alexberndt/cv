7538be835ed8ba09f5aa0703d4c41f00b62ea259
Alex Berndt <alex@overstory.com>
2023-11-12 20:25:40 +0100

    (chore): Refactored home bio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,40 @@
 <template>
   <div class="home">
     <p>
-      I am a machine learning engineer at 
-        <a href="https://www.overstory.com/">Overstory.ai</a> 
+      Software and data engineer working at 
+        <a href="https://www.overstory.com/">Overstory</a> 
       where I work on solving a wide variety of geospatial-related problems.
-      My main focus is on designing, implementing and orchestrating 
-      machine learning pipelines.
+      My contributions include algorithm optimization
+        e.g. using parallel processing,
+        maintaining infrastructure and resource provisioning,
+        
     </p>
+    <h4>Interests</h4>
+    <p>In my role as a software engineer, my current interests include</p>
+    <ul>
+      <li>Algorithm optimization using distributed tools like <a href="https://www.dask.org/">Dask</a>, and improved <a href="https://kubernetes.io/">K8s</a> provisioning,</li>
+      <li>Developing, maintaining and orchestrating data pipelines using <a href="https://dagster.io/">Dagster</a>,</li>
+      <li>Interfacing business value and algorithm optimization functions.</li>
+    </ul>
+    <p>Some non-work related interests include</p>
+    <ul>
+      <li>Chess,</li>
+      <li>Playing the piano or guitar,</li>
+      <li>Outdoor activities like mountain biking, skiing, hiking.</li>
+    </ul>
     <h4>Education</h4>
     <p>
       I obtained my Masters degree in Systems & Control at the 
       <a href="https://www.tudelft.nl/3me/over/afdelingen/delft-center-for-systems-and-control">Delft Center for Systems and Control</a>
        of <a href="https://www.tudelft.nl/en/">TU Delft</a>, 
         the Netherlands.
-      My master's thesis was supported by the 
-      <a href="https://www.bosch-ai.com/">Robert Bosch Research Campus</a> in Renningen, Germany, 
-      involved the development of a 
-      <a href="https://repository.tudelft.nl/islandora/object/uuid%3Ad70c13ac-a992-4408-a7db-30033eec987d"> novel real-time optimization scheme</a>
-       to re-order autonomous guided vehicles 
-       (AGVs) in collaborative warehouses.
+      My thesis involved the development of a 
+      <a href="https://repository.tudelft.nl/islandora/object/uuid%3Ad70c13ac-a992-4408-a7db-30033eec987d">novel, real-time optimization scheme</a>
+      to re-order autonomous guided vehicles (AGVs) in human-and-robot warehouses.
+      This project was supported by and completed on-site at the 
+      <a href="https://www.bosch-ai.com/">Robert Bosch Research Campus</a> in Renningen, Germany.
     </p>
-    <p>I also spent 9 months at the 
+    <p>Furthermore, I spent 9 months at the 
       <a href="https://www.kth.se/en">KTH Royal Institute of Technology</a>
       in Stockholm, Sweden, as a PhD student under the supervision of 
       <a href="https://people.kth.se/~hsan/">Henrik Sandberg</a>
@@ -34,21 +48,6 @@
       <a href="https://ptolemy.berkeley.edu/projects/cps/">cyber-physical systems</a>,
       after which I ended up pursuing a career in industry.
     </p>
-    <h4>Interests</h4>
-    <p>I am a generally curious person and interested in a wide variety of topics.
-      In my current role as a machine learning engineer, I am particularly interested in
-    </p>
-    <ul>
-      <li>Productization of ML-based pipelines,</li>
-      <li>Designing and optimizing data-driven processes,</li>
-      <li>Interfacing business value and algorithm optimization functions.</li>
-    </ul>
-    <p>Other non-work related interests include</p>
-    <ul>
-      <li>Full-stack web development,</li>
-      <li>A casual chess game,</li>
-      <li>Playing the piano.</li>
-    </ul>
   </div>
 </template>
 
