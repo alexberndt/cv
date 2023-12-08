fd1811350027101650f46d472102d2902c52e2c1
Alex Berndt <alex@overstory.com>
2023-12-08 18:19:50 +0100

    (chore): Add CV

---
+++ src/views/CV.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="cv">
+        <h1>Hello, CV</h1>
+        <!-- <pdf src="cv/latest.pdf"></pdf> -->
+    </div>
+  </template>
+  
+  <script>
+//   import pdf from 'vue-pdf'
+  
+  export default {
+    name: 'CV',
+    components: {}
+  }
+  </script>
+  
+  <style scoped lang="scss">
+  h4 {
+    text-align: center;
+    padding: 20px;
+  }
+  
+  .experience {
+    text-align: left;
+  }
+  
+  .container {
+    max-width: 768px;
+    margin: auto;
+    overflow: auto;
+  }
+  </style>
+  
